3af1a5acf40af28c6310280942686d85ccf2cc64
Akash <akashbbu@gmail.com>
2024-01-19 18:04:48 +0530

    Build Buttton component include validation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import axios from './axios'
 import { useEffect, useState, useRef } from 'react'
 import ScheduleList from './compnents/ScheduleList/ScheduleList'
 import ScheduleForm from './compnents/ScheduleForm/ScheduleForm'
+import Button from './compnents/Button/Button'
 import type { Schedule } from './types'
 
 const App: React.FC = () => {
@@ -19,7 +20,6 @@ const App: React.FC = () => {
     try {
       const res = await axios.get(`/schedules?title=${term}`)
       setSchedules(res.data);
-      //setFilteredSchedules(res.data)
       
     } catch (error) {
       console.log('could fetch data', error)
@@ -77,9 +77,11 @@ const App: React.FC = () => {
           {/* display list of component  */}
           <div className='dashboardSearch-fields'>
             <SearchBox onSearch={(term) => setTerm(term)}/>
-            <button className='btn' onClick={addNew}>
-            <svg style={{width: '20px', height: '20px'}}xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" id="add"><g fill="#fff"><path d="M32 55.9C18.8 55.9 8.1 45.2 8.1 32S18.8 8.1 32 8.1 55.9 18.8 55.9 32 45.2 55.9 32 55.9zm0-45.2c-11.7 0-21.3 9.6-21.3 21.3S20.3 53.3 32 53.3 53.3 43.7 53.3 32 43.7 10.7 32 10.7z"></path><path d="M19.3 30.6h25.4v2.8H19.3z"></path><path d="M30.6 19.3h2.8v25.4h-2.8z"></path></g></svg>
-               <span>Add</span></button>
+            <Button variant="secondary"  onClick={addNew}>
+               <svg style={{width: '20px', height: '20px'}}xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" id="add"><g fill="#fff"><path d="M32 55.9C18.8 55.9 8.1 45.2 8.1 32S18.8 8.1 32 8.1 55.9 18.8 55.9 32 45.2 55.9 32 55.9zm0-45.2c-11.7 0-21.3 9.6-21.3 21.3S20.3 53.3 32 53.3 53.3 43.7 53.3 32 43.7 10.7 32 10.7z"></path><path d="M19.3 30.6h25.4v2.8H19.3z"></path><path d="M30.6 19.3h2.8v25.4h-2.8z"></path></g></svg>
+               <span>Add</span>
+            </Button>
+           
             {popoverForm}
 
           </div> 
